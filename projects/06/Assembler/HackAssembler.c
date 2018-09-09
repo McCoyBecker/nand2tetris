6727ee966cddf67ad94680e53283b81549733d1e
McCoy <mccoybecker@gmail.com>
2018-09-09 08:46:19 -0700

    Beginning work on symbol table implementation

--- projects/06/Assembler/HackAssembler.c
+++ projects/06/Assembler/HackAssembler.c
@@ -4,11 +4,52 @@
 #include <string.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdbool.h>
 
 /* Declare functions. */
 
+char *binaryExpansion(int input);
 void translateLine(char input_line[], FILE *output_file);
-void dictionaryLookUp(char input[], char output[]);
+void translate(char input[], char output[]);
+char *symbolTable(char input[], bool create);
+
+/* A symbol table to handle predefined symbols and user-create symbols for the Hack machine. */
+
+char *symbolTable(char input[], bool create)
+{
+    char *RAMaddress = NULL;
+
+    if (strcmp(input, "SP"))
+    {
+        RAMaddress = (char*) calloc(1,sizeof(char));
+        memcpy(RAMaddress, "0", 1);
+    }
+
+    if (strcmp(input, "LCL"))
+    {
+        RAMaddress = (char*) calloc(1,sizeof(char));
+        memcpy(RAMaddress, "1", 1);
+    }
+
+    if (strcmp(input, "ARG"))
+    {
+        RAMaddress = (char*) calloc(1,sizeof(char));
+        memcpy(RAMaddress, "2", 1);
+    }
+
+    if (strcmp(input, "THIS"))
+    {
+        RAMaddress = (char*) calloc(1,sizeof(char));
+        memcpy(RAMaddress, "3", 1);
+    }
+
+    if (strcmp(input, "THAT"))
+    {
+        RAMaddress = (char*) calloc(1,sizeof(char));
+        memcpy(RAMaddress, "4", 1);
+    }
+    return(RAMaddress);
+}
 
 /* A function to determine the binary expansion of an integer. Returns the binary expansion in string form. */
 
@@ -36,7 +77,7 @@ char *binaryExpansion(int input)
 
 /* Translation using lookup. */
 
-void dictionaryLookUp(char input[], char output[])
+void translate(char input[], char output[])
 {
     for(int i = 0; i < strlen(input); i++)
     {
@@ -74,7 +115,7 @@ void translateLine(char line_to_translate[], FILE *output)
 {
     int len_input = strlen(line_to_translate);
     char output_line[17]; // Hack is a 16-bit platform. 17-th spot is for newlines.
-    dictionaryLookUp(line_to_translate, output_line);
+    translate(line_to_translate, output_line);
     fputs(output_line, output);
 }
 
@@ -87,12 +128,14 @@ int main()
     char filename[1000];
     printf("What file should I translate?\n");
     scanf("%s", filename);
-
+    char namecopy[strlen(filename)];
+    memcpy(namecopy, filename, strlen(filename));
+    
     /* Read in input file, generate output file. */
 
     FILE *output, *input;
     input = fopen(strcat(filename,".asm"), "r");
-    output = fopen("AssembledOutput.asm", "w+");
+    output = fopen(strcat(namecopy, ".hack"), "w+");
 
     /* Line by line translation. */
 
---
+++ projects/06/Assembler/symbol_table/symboltable.c
@@ -0,0 +1,7 @@
+/* A symbol table implementation with user-defined entries. */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Declare functions. */
